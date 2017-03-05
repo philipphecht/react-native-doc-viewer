5cc77b530f69fd25aebe2c07e2357150db85c816
Philipp Hecht <philipp.hecht@bmtg.ch>
2017-03-05 16:44:27 +0100

    Version

--- ios/RNReactNativeDocViewer.m
+++ ios/RNReactNativeDocViewer.m
@@ -4,11 +4,6 @@
 
 @implementation RNReactNativeDocViewer
 
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(open: (NSURL *)path)
