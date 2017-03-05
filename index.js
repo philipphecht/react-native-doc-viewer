638989cd21c2a65b3f381d04313c0cba1343f294
Philipp Hecht <philipp.hecht@bmtg.ch>
2017-03-05 16:41:04 +0100

    Functions

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import { NativeModules } from 'react-native';
 const RNReactNativeDocViewer = NativeModules.RNReactNativeDocViewer;
 export default {
   open: RNReactNativeDocViewer.open,
-  addEvent:open: RNReactNativeDocViewer.addEvent
+  addEvent: RNReactNativeDocViewer.addEvent
 }
