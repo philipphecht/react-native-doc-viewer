b816b44564b1ef02b3a70d4954c3983e8f1c6e65
Philipp Hecht <philipp.hecht@bmtg.ch>
2017-03-05 14:08:13 +0100

    Implementation Interaction Controller Document

--- ios/RNReactNativeDocViewer.m
+++ ios/RNReactNativeDocViewer.m
@@ -1,13 +1,28 @@
 
+#import <UIKit/UIKit.h>
 #import "RNReactNativeDocViewer.h"
 
 @implementation RNReactNativeDocViewer
+
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(open: (NSURL *)path)
+{
+    UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
+    interactionController.delegate = self;
+    [interactionController presentPreviewAnimated:YES];
+}
+
+- (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
+{
+    return [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+}
+
 
 @end
   
