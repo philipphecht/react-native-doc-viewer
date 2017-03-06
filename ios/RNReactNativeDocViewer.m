68d7f24d283d598c679f88671fc0e9433c75c612
Philipp Hecht <philipp.hecht@bmtg.ch>
2017-03-06 08:26:22 +0100

    methodQueue

--- ios/RNReactNativeDocViewer.m
+++ ios/RNReactNativeDocViewer.m
@@ -12,6 +12,12 @@
 
 RCT_EXPORT_MODULE()
 
+- (dispatch_queue_t)methodQueue
+{
+  return dispatch_get_main_queue();
+}
+
+
 RCT_EXPORT_METHOD(open: (NSURL *)path)
 {
     UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
