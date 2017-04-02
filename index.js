3846040ce3f3397829ecef386aafe90faee5a043
Philipp Hecht <philipp.hecht@bmtg.ch>
2017-04-03 01:24:18 +0200

    Function Playmovie

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const RNReactNativeDocViewer = NativeModules.RNReactNativeDocViewer;
 export default {
   openDoc: RNReactNativeDocViewer.openDoc,
   openDocb64: RNReactNativeDocViewer.openDocb64,
+  playMovie: RNReactNativeDocViewer.playMovie,
   testModule: RNReactNativeDocViewer.testModule
 }
